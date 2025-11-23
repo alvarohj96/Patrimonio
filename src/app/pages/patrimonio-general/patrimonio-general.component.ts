b65b59353383d0a56a08feef38f84458b103106d
Alvaro <alvarohj96sg@gmail.com>
2025-11-23 22:27:15 +0100

    Added styles in patrimonio-general

--- src/app/pages/patrimonio-general/patrimonio-general.component.ts
+++ src/app/pages/patrimonio-general/patrimonio-general.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule, DecimalPipe } from '@angular/common';
-import { PatrimonioService } from '../../services/patrimonio.service';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-patrimonio-general',
   standalone: true,
-  imports: [CommonModule, DecimalPipe],
+  imports: [
+    CommonModule,
+    DecimalPipe,
+    MatCardModule,
+    MatTableModule
+  ],
   templateUrl: './patrimonio-general.component.html',
   styleUrls: ['./patrimonio-general.component.scss']
 })
@@ -14,10 +20,28 @@ export class PatrimonioGeneralComponent implements OnInit {
   patrimonio: any[] = [];
   total = 0;
 
-  constructor(private patrimonioService: PatrimonioService) {}
+  columnas: string[] = ['categoria', 'total'];
+
+  constructor() {}
 
   ngOnInit(): void {
-    this.patrimonio = this.patrimonioService.getPatrimonioGeneral();
+    // ⚠ Aquí debes leer del servicio real cuando lo conectemos
+    // De momento lo dejo preparado:
+    const stored = localStorage.getItem('movimientos');
+    const movimientos = stored ? JSON.parse(stored) : [];
+
+    const agregados: any = {};
+
+    movimientos.forEach((m: any) => {
+      if (!agregados[m.categoria]) agregados[m.categoria] = 0;
+      agregados[m.categoria] += m.valor;
+    });
+
+    this.patrimonio = Object.keys(agregados).map(key => ({
+      categoria: key,
+      total: agregados[key]
+    }));
+
     this.total = this.patrimonio.reduce((s, p) => s + p.total, 0);
   }
 }
